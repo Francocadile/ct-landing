b5041a46e737217e4dde0d5586a62fd983921ab8
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-08-17 13:13:04 -0300

    feat: connect Home page in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
+import Home from "./pages/Home";
+
 export default function App() {
   return (
-    <div
-      style={{
-        padding: "40px",
-        fontFamily:
-          "system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial, sans-serif",
-      }}
-    >
-      <h1>¡Funciona!</h1>
-      <p>Si ves esto, React está renderizando OK.</p>
+    <div>
+      <Home />
     </div>
   );
 }
