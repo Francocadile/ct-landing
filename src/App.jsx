db2393bda71bbccdf76d05d7366523c1d425e599
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-08-17 16:32:38 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Filosofia from "./sections/Filosofia";
 import Experiencia from "./sections/Experiencia";
+import Blog from "./sections/Blog";
 import Contacto from "./sections/Contacto";
 import Home from "./Home";
 
@@ -14,6 +15,7 @@ export default function App() {
       <Filosofia />
       <Experiencia />
       <Home />
+      <Blog />
       <Contacto />
       <Footer />
     </>
