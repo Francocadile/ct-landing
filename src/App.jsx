59a1f3980e8dc66e22ad0ca4c09e0608cadc98aa
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-08-17 16:12:20 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+// src/App.jsx
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Filosofia from "./sections/Filosofia";
 import Home from "./Home";
 
 export default function App() {
   return (
     <>
       <Header />
+      <Filosofia />
       <Home />
       <Footer />
     </>
