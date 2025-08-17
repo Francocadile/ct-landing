59a57451027fd1d3b4bc1663a5992e07e2491f3e
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-08-17 13:24:15 -0300

    Create Home.jsx

---
+++ src/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24, maxWidth: 960, margin: "0 auto" }}>
+      <h1>¡Home lista!</h1>
+      <p>Si ves esto, la importación <code>./pages/Home</code> funciona.</p>
+    </main>
+  );
+}
