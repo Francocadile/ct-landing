b9ab8335956ec320269e6c99273acc42e138bba1
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-08-18 15:11:17 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 // src/App.jsx
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Home from "./Home.jsx";
+import StaffProfile from "./pages/StaffProfile.jsx";
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Home />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/staff/:slug" element={<StaffProfile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
