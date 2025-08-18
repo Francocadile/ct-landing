c647c5b220ffa9a8b523d032cc840d255dfdbe61
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-08-18 12:13:24 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,8 @@
 // src/App.jsx
 import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-
-// Home contiene Hero (#home) + Staff (#staff)
-import Home from "./Home";
-import Experiencia from "./sections/Experiencia";
-import Resultados from "./sections/Resultados";   // NUEVO
-import Modelo from "./sections/Modelo";
-import Blog from "./sections/Blog";
-import Contacto from "./sections/Contacto";
+import Home from "./Home.jsx";
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <Home />
-      <Experiencia />
-      <Resultados />
-      <Modelo />
-      <Blog />
-      <Contacto />
-      <Footer />
-    </>
-  );
+  return <Home />;
 }
 
