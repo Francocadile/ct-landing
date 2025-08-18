1509fba2162b9b3ed33e0c4c1f7516ac3aaf9a9d
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-08-18 13:22:48 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 // src/App.jsx
 import React from "react";
-import Header from "./components/Header.jsx"; // <- volvemos a montar el menú
+import { Routes, Route, Navigate } from "react-router-dom";
+import Header from "./components/Header.jsx";
 import Home from "./Home.jsx";
 
 export default function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Cualquier ruta desconocida vuelve al Home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </>
   );
 }
