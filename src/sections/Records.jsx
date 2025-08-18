5486c6ae961716cb84f4c89f29dc0801b77fee1c
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-08-18 11:26:49 -0300

    Create Records.jsx

---
+++ src/sections/Records.jsx
@@ -0,0 +1,4 @@
+// src/sections/Records.jsx
+// Reexporta el componente existente de Resultados pero con el nombre nuevo "Records".
+import Resultados from "./Resultados.jsx";
+export default Resultados;
