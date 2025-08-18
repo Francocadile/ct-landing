245322ce0a19a8583bb20d05c5537c1cf75c5685
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-08-18 14:48:23 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 // src/App.jsx
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Home from "./Home.jsx";
-import Member from "./pages/Member.jsx";
 
 export default function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/staff/:slug" element={<Member />} />
-        {/* Cualquier ruta desconocida vuelve al Home */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <Home />
     </>
   );
 }
