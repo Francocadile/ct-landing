b52a6210f588508a4117e7eff7a6553eda97cf61
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-08-18 14:58:01 -0300

    Create staff-pages.js

---
+++ src/data/staff-pages.js
@@ -0,0 +1,50 @@
+// src/data/staff-pages.js
+// Mapa de clubes por integrante. Los logos deben existir en /public/img/clubs/
+
+export const CLUBS_BY_MEMBER = {
+  flavio: [
+    { name: "Bolívar", logo: "/img/clubs/bolivar.png" },
+    { name: "Atlético Huila", logo: "/img/clubs/atletico-huila.png" },
+    { name: "Nacional Potosí", logo: "/img/clubs/nacional-potosi.png" },
+    { name: "Mitre", logo: "/img/clubs/mitre.png" },
+    { name: "Millonarios", logo: "/img/clubs/millonarios.png" },
+    { name: "Liga de Loja", logo: "/img/clubs/loja.png" },
+    { name: "Jaguares FC", logo: "/img/clubs/jaguares-fc.png" },
+    { name: "Cúcuta Deportivo", logo: "/img/clubs/cucuta-deportivo.png" },
+    { name: "Barcelona SC", logo: "/img/clubs/barcelona-sc.png" },
+    { name: "Alianza Atlético", logo: "/img/clubs/alianza-atletico.png" },
+  ],
+
+  sandro: [
+    { name: "Bolívar", logo: "/img/clubs/bolivar.png" },
+    { name: "Nacional Potosí", logo: "/img/clubs/nacional-potosi.png" },
+    { name: "Atenas", logo: "/img/clubs/atenas.png" },
+    { name: "Renato", logo: "/img/clubs/renato.jpg" }, // ← .jpg según tu carpeta
+  ],
+
+  horacio: [
+    { name: "Bolívar", logo: "/img/clubs/bolivar.png" },
+    { name: "Estudiantes", logo: "/img/clubs/estudiantes.png" },
+  ],
+
+  juan: [
+    { name: "Bolívar", logo: "/img/clubs/bolivar.png" },
+  ],
+
+  franco: [
+    { name: "Deportivo Táchira", logo: "/img/clubs/tachira.png" },
+    { name: "Las Chankas", logo: "/img/clubs/chankas.png" },
+    { name: "Gimnasia", logo: "/img/clubs/gimnasia.png" },
+    // Ojo con el nombre del archivo: nos diste "real-potosí.png" (con tilde)
+    { name: "Real Potosí", logo: "/img/clubs/real-potosí.png" },
+    { name: "Atlético Huila", logo: "/img/clubs/atletico-huila.png" },
+    { name: "Nacional Potosí", logo: "/img/clubs/nacional-potosi.png" },
+    { name: "Juventud", logo: "/img/clubs/juventud.png" },
+  ],
+
+  gabriel: [
+    { name: "Bolívar", logo: "/img/clubs/bolivar.png" },
+    { name: "Nacional Potosí", logo: "/img/clubs/nacional-potosi.png" },
+    { name: "Zulia", logo: "/img/clubs/zulia.png" },
+  ],
+};
