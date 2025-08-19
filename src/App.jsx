a72822331d0070d02a1389bc9f809d22ecc699de
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-08-19 11:47:06 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 // src/App.jsx
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Header from "./components/Header.jsx";
 import Home from "./Home.jsx";
 import StaffProfile from "./pages/StaffProfile.jsx";
-
-// NUEVO: páginas del blog
 import BlogIndex from "./pages/BlogIndex.jsx";
 import BlogPost from "./pages/BlogPost.jsx";
 
@@ -15,16 +12,12 @@ export default function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        {/* Home */}
         <Route path="/" element={<Home />} />
-
-        {/* Staff (detalle por slug) */}
         <Route path="/staff/:slug" element={<StaffProfile />} />
-
-        {/* Blog (listado y detalle) */}
         <Route path="/blog" element={<BlogIndex />} />
         <Route path="/blog/:slug" element={<BlogPost />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
