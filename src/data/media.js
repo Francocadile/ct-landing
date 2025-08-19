8a789d34ef7d606882dbb5f790c674d577f157ea
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-08-19 13:00:36 -0300

    Create media.js

---
+++ src/data/media.js
@@ -0,0 +1,27 @@
+// src/data/media.js
+export const MEDIA = [
+  { title: "Metodología Integrada Estructural", url: "https://www.youtube.com/watch?v=0bT2Wv8_Rdo" },
+  { title: "Plan de partido", url: "https://www.youtube.com/watch?v=FmmW6_ZYaSg" },
+  { title: "Desarrollo conceptual de nuestra idea de juego", url: "https://www.youtube.com/watch?v=ttGk4H6FL20" },
+  { title: "Modelo de juego (inicio)", url: "https://www.youtube.com/watch?v=tbKldwnHWmc" },
+  { title: "Modelo de juego (posesiones)", url: "https://www.youtube.com/watch?v=F6I96TJpvCw" },
+  { title: "Modelo de juego (patrones ofensivos)", url: "https://www.youtube.com/watch?v=QkuAaSEf7GQ" },
+
+  { title: "Entrenamientos: Partido condicionado en ataque", url: "https://www.youtube.com/watch?v=Z_h0p1yDFWw&t=22s" },
+  { title: "Entrenamientos: Juego de progresión por zona", url: "https://www.youtube.com/watch?v=X_9X728zkxM" },
+  { title: "Entrenamientos: 5 cuadrados + 2 zonas", url: "https://www.youtube.com/watch?v=wtLKrrRXAg4" },
+
+  { title: "Objetivos cumplidos", url: "https://www.youtube.com/watch?v=JimH9te95Ls&list=PLKtocnQP3WlnHgU_TlxZ0Cu9wiZxF4Lg-&index=9" },
+
+  { title: "Microciclo estándar: aspectos defensivos", url: "https://www.youtube.com/watch?v=fjqWrHxEoAg&list=PLKtocnQP3WlnHgU_TlxZ0Cu9wiZxF4Lg-&index=3" },
+  { title: "Jugadas iniciadas desde los costados", url: "https://www.youtube.com/watch?v=HFFmaTCdGw0&list=PLKtocnQP3WlnHgU_TlxZ0Cu9wiZxF4Lg-&index=5" },
+  { title: "Salidas", url: "https://www.youtube.com/watch?v=BQQ1VYTgH-g&list=PLKtocnQP3WlnHgU_TlxZ0Cu9wiZxF4Lg-&index=6" },
+  { title: "Juego de posesión", url: "https://www.youtube.com/watch?v=zQszCfbgvns" },
+  { title: "Ataques posicionales", url: "https://www.youtube.com/watch?v=LX8TJ2gHBDE" },
+
+  { title: "Bitácora de entrenamiento 2025 (playlist)", url: "https://www.youtube.com/playlist?list=PLKtocnQP3Wll2sMfDid65Ne5jiokEjPNV" },
+  { title: "Bitácora de entrenamiento 2024 (playlist)", url: "https://www.youtube.com/playlist?list=PLKtocnQP3WlkQMZnut9tvWXPmO9QFggEM" },
+
+  { title: "Pretemporada", url: "https://www.youtube.com/watch?v=qcB9qLSU9QI" },
+  { title: "Goles", url: "https://www.youtube.com/watch?v=ZlBX64mE094" },
+];
